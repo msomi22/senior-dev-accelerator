3f2fd2851e78f658d4b19bfe7218a29988c90da2
Peter Mwenda <mwendapeter72@gmail.com>
2026-05-19 20:43:08 +0300

    Migrate bit manipulation problems to problem files

---
+++ src/data/problems/dsa/bit-manipulation/bit-manipulation-004.js
@@ -0,0 +1,8 @@
+import topic from '../../../banks/dsa/bit-manipulation.js';
+
+const problem = {
+  ...topic.questions.find((question) => question.id === 'bit-manipulation-004'),
+  category: 'dsa'
+};
+
+export default problem;
