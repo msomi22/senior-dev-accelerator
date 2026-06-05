f59e7626127b7c52d3376757850242684342d02a
Peter Mwenda <mwendapeter72@gmail.com>
2026-06-05 03:33:53 +0300

    feat(renderers): add default question renderer wrapper

---
+++ src/components/question-renderers/default/DefaultQuestionRenderer.jsx
@@ -0,0 +1,5 @@
+import FocusedProblemWorkspace from '../../FocusedProblemWorkspace.jsx';
+
+export default function DefaultQuestionRenderer(props) {
+  return <FocusedProblemWorkspace {...props} />;
+}
