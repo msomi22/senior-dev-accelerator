843318353d33ae6569c22487a31517bdcf691708
Peter Mwenda <mwendapeter72@gmail.com>
2026-06-08 22:00:53 +0300

    Delete src/config/academies/c.ts

--- src/config/academies/c.ts
+++
@@ -1,1 +0,0 @@
-export const c={id:'cbc',displayName:'CBC Academy',productName:'CBC Academy',subdomains:['cbc.academy.qubitel.net'],storageKey:'qubitel-academy:v2:cbc',categoryIds:['grade-1','grade-3']};
