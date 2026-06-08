e9b98d88a56193f52ee9085a9add605034a9ffcd
Peter Mwenda <mwendapeter72@gmail.com>
2026-06-08 21:22:43 +0300

    fix: add compact cbc academy config

---
+++ src/config/academies/c.ts
@@ -0,0 +1,1 @@
+export const c={id:'cbc',displayName:'CBC Academy',productName:'CBC Academy',subdomains:['cbc.academy.qubitel.net'],storageKey:'qubitel-academy:v2:cbc',categoryIds:['grade-1','grade-3']};
