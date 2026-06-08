a473e804e7875ad9ef8759fa164d4171adb65cc2
Peter Mwenda <mwendapeter72@gmail.com>
2026-06-08 21:27:47 +0300

    fix: re-export compact academy registry

--- src/config/academyRegistry.ts
+++ src/config/academyRegistry.ts
@@ -1,5 +1,2 @@
-import{t}from'./academies/t.ts';import{c}from'./academies/c.ts';import{x}from'./academies/x.ts';
-export const DEFAULT_ACADEMY_ID='tech';
-export const academyRegistry={tech:t,cbc:c,'customer-experience':x};
-export function getAcademyById(a=DEFAULT_ACADEMY_ID){return academyRegistry[a]||academyRegistry[DEFAULT_ACADEMY_ID]}
-export function getDefaultAcademy(){return
+export{DEFAULT_ACADEMY_ID,getAcademyById,getDefaultAcademy,getAcademies}from'./academies/r.ts';
+export{r as academyRegistry}from'./academies/r.ts';
